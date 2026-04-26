a246327c2cc218010175cf1fc78abbaa060b1687
Daniel Mariz <danielmrz.dev@gmail.com>
2026-04-26 16:07:11 -0300

    Configuração inicial

---
+++ src/main/java/br/com/fiap/finconnect/FinconnectApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.finconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinconnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinconnectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/finconnect/FinconnectApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.finconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinconnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
