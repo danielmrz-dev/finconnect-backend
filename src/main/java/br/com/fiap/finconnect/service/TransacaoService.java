da8f13ba25569f5864fb3c7ac91791d5458781c2
Daniel Mariz <danielmrz.dev@gmail.com>
2026-05-09 14:11:21 -0300

    ajusta método de listagem para retornar lista de transações ordenada por data

--- src/main/java/br/com/fiap/finconnect/service/TransacaoService.java
+++ src/main/java/br/com/fiap/finconnect/service/TransacaoService.java
@@ -7,6 +7,7 @@ import br.com.fiap.finconnect.repository.TransacaoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 
@@ -21,7 +22,9 @@ public class TransacaoService {
   }
 
   public List<Transacao> listaTransacoes() {
-    return transacaoRepository.findAll();
+    List<Transacao> foundList = transacaoRepository.findAll();
+    foundList.sort(Comparator.comparing(Transacao::getData));
+    return foundList;
   }
 
   public Transacao buscarTransacaoPorId(Long id) {
